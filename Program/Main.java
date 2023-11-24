a5d3a68c5b8b777accd196abf3c82ad356e01652
wellington-alencar <130476068+wellington-alencar@users.noreply.github.com>
2023-11-23 23:50:23 -0300

    Update Main.java

--- Program/Main.java
+++ Program/Main.java
@@ -9,16 +9,13 @@ public class Main {
         LocalDate dataAtual = LocalDate.now();
         int nascimento = Integer.parseInt(JOptionPane.showInputDialog("Digite sua data de nascimento (ex: 10072000 ddMMyyy)"));
 
-        // here i'm to coverting the nascimento for String
         String numeroDaData = String.valueOf(nascimento);
 
-        //here i'm to  separate Days(str), Mounth(str) and Years
         Integer dia = Integer.parseInt(numeroDaData.substring(0,2));
         Integer mes = Integer.parseInt(numeroDaData.substring(2,4));
         Integer ano = Integer.parseInt(numeroDaData.substring(4, 8));
 
 
-        //here i'm to assigning the date for one LocalDate
         LocalDate nascimentoFormatado = LocalDate.of(ano, mes, dia);
 
         Period diferenca = Period.between( nascimentoFormatado, dataAtual);
